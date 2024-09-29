e93c281a7788e5fab966c699aae461b09b2aa972
Miguel Silva <miguelcesilva@gmail.com>
2024-09-30 00:04:34 +0100

    Update neotest to autodecide runner

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -4,7 +4,6 @@ return {
     "nvim-neotest/nvim-nio",
     "nvim-neotest/neotest-python",
     "nvim-neotest/neotest-plenary",
-    "nvim-neotest/neotest-vim-test",
   },
   config = function()
     require("neotest").setup({
@@ -12,12 +11,8 @@ return {
         require("neotest-python")({
           dap = { justMyCode = false },
           python = ".venv/bin/python",
-          runner = "unittest",
         }),
         require("neotest-plenary"),
-        require("neotest-vim-test")({
-          ignore_file_types = { "python", "vim", "lua" },
-        }),
       },
     })
   end,
