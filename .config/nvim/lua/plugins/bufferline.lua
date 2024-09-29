252a44ce013dbb3f17bb9b1ac06aebf23403fa7c
Miguel Silva <miguelcesilva@gmail.com>
2024-09-29 23:14:09 +0100

    Fix closing of current buffer

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -7,7 +7,13 @@ return {
       "<Cmd>BufferLineGroupClose ungrouped<CR>",
       desc = "[B]uffer Delete Non-[P]inned",
     },
-    { "<leader>bd", "<Cmd>BufferLinePickClose<CR>", desc = "[B]uffer [D]elete" },
+    {
+      "<leader>bd",
+      function()
+        vim.cmd("bd")
+      end,
+      desc = "[B]uffer [D]elete",
+    },
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "[B]uffer Delete [O]thers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "[B]uffer Delete [R]ight" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "[B]uffer Delete [L]eft" },
