bbd9f05a4c22e6c9ffbdea98a2f4fb4e9491df3e
Miguel Silva <miguelcesilva@gmail.com>
2024-10-10 08:44:53 +0100

    Add treesitter-context

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,20 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "json",
-        "lua",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "toml",
-        "yaml",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "json",
+          "lua",
+          "html",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "toml",
+          "yaml",
+        },
+        auto_install = true,
+        highlight = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }
