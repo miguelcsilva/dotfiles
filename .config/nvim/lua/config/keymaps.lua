753631d71efba8eec13d487abd736a9054a3a6ab
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2024-11-28 09:30:20 +0000

    Update keybindings (#23)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,18 +1,28 @@
+-- Basic
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":q!<CR>")
 
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
-
-vim.keymap.set("n", "<M-h>", "<cmd>vertical resize -2<cr>", { desc = "Shrink window horizontally" })
-vim.keymap.set("n", "<M-l>", "<cmd>vertical resize +2<cr>", { desc = "Enlarge window vertically" })
-vim.keymap.set("n", "<M-k>", "<cmd>resize +2<cr>", { desc = "Enlarge window vertically" })
-vim.keymap.set("n", "<M-j>", "<cmd>resize -2<cr>", { desc = "Shrink window vertically" })
-
-vim.keymap.set("n", "sv", ":vsplit<Return>", { desc = "Split vertically" })
-vim.keymap.set("n", "ss", ":hsplit<Return>", { desc = "Split horizontally" })
-
+-- Visual mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent lines left" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent lines right" })
+
+-- Jumping windows
+vim.keymap.set("n", "<C-w><Left>", "<C-w>h", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-w><Right>", "<C-w>l", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-w><Down>", "<C-w>j", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-w><Up>", "<C-w>k", { desc = "Move focus to the upper window" })
+
+-- Resizing windows
+vim.keymap.set("n", "<M-h>", "<cmd>vertical resize -2<cr>", { desc = "Shrink window horizontally" })
+vim.keymap.set("n", "<M-l>", "<cmd>vertical resize +2<cr>", { desc = "Enlarge window horizontally" })
+vim.keymap.set("n", "<M-j>", "<cmd>resize -2<cr>", { desc = "Shrink window vertically" })
+vim.keymap.set("n", "<M-k>", "<cmd>resize +2<cr>", { desc = "Enlarge window vertically" })
+vim.keymap.set("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Shrink window horizontally" })
+vim.keymap.set("n", "<M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Enlarge window horizontally" })
+vim.keymap.set("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Shrink window vertically" })
+vim.keymap.set("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Enlarge window vertically" })
+
+-- Splitting windows
+vim.keymap.set("n", "sv", ":vsplit<Return>", { desc = "Split vertically" })
+vim.keymap.set("n", "ss", ":split<Return>", { desc = "Split horizontally" })
