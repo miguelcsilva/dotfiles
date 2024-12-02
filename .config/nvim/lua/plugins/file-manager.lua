c4c408e03a9029fead7376958f97ce008cc60199
Miguel Silva <miguelcesilva@gmail.com>
2024-12-02 09:59:46 +0000

    Add yazi

---
+++ .config/nvim/lua/plugins/file-manager.lua
@@ -0,0 +1,12 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      -- Open in the current working directory
+      "<leader>e",
+      "<cmd>Yazi cwd<cr>",
+      desc = "File [E]xplorer",
+    },
+  },
+}
