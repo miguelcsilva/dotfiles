86618827baaae9933dc98905bc10eb0872e717a0
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2024-12-03 10:43:39 +0000

    Add telescope file manager (#30)

--- .config/nvim/lua/plugins/file-manager.lua
+++ .config/nvim/lua/plugins/file-manager.lua
@@ -1,12 +1,25 @@
 return {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   keys = {
-    {
-      -- Open in the current working directory
-      "<leader>e",
-      "<cmd>Yazi cwd<cr>",
-      desc = "File [E]xplorer",
-    },
+    { "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", mode = "n", desc = "File [E]xplorer" },
   },
+  config = function()
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    require("telescope").setup({
+      defaults = { --[[ your defaults]]
+      },
+      extensions = {
+        file_browser = {
+          ["n"] = {
+            c = fb_actions.create,
+            r = fb_actions.rename,
+            m = fb_actions.move,
+            y = fb_actions.copy,
+          },
+        },
+      },
+    })
+  end,
 }
