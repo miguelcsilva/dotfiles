946c5c83794579baee3d37020710572309e1e525
Miguel Silva <miguelcesilva@gmail.com>
2024-12-18 10:04:22 +0000

    Update noice

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,22 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    presets = {
+      long_message_to_split = true, -- long messages will be sent to a split
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
   },
 }
