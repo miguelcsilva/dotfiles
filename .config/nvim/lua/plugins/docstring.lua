fc8e093d9127887621dfc948ce41424607ed66f3
Miguel Silva <miguelcesilva@gmail.com>
2025-01-15 11:24:10 +0000

    Add doge

---
+++ .config/nvim/lua/plugins/docstring.lua
@@ -0,0 +1,9 @@
+return {
+  "kkoomen/vim-doge",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":call doge#install()", -- add to be run manually
+  config = function()
+    vim.g.doge_doc_standard_python = "google"
+    vim.keymap.set("n", "<leader>co", "<Plug>(doge-generate)", { desc = "[C]ode d[O]cstring" })
+  end,
+}
