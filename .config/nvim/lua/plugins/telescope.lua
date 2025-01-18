11fff414b4e2fce475304879f1cc2b56f5ec2e20
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2025-01-18 20:40:22 +0000

    Expand telescope search (#50)

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,13 +31,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
         buffers = {
           show_all_buffers = true,
           sort_lastused = true,
+          theme = "dropdown",
           previewer = true,
           mappings = {
             i = {
-              ["<C-d>"] = "delete_buffer",
-            },
-            n = {
-              ["d"] = "delete_buffer",
+              ["<c-d>"] = "delete_buffer",
             },
           },
         },
@@ -51,37 +49,27 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(require("telescope").load_extension, "ui-select")
 
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    local dropdown = require("telescope.themes").get_dropdown({ winblend = 10, previewer = false })
+    -- stylua: ignore start
+    -- Files/Buffers
+    vim.keymap.set("n", "<leader><leader>", function() builtin.buffers(dropdown) end, { desc = "Search Buffers" })
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-    vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "[S]earch [R]ecent" })
-    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Search Buffers" })
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-      }))
-    end, { desc = "[/] Fuzzily search in current buffer" })
-
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set("n", "<leader>s/", function()
-      builtin.live_grep({
-        grep_open_files = true,
-        prompt_title = "Live Grep in Open Files",
-      })
-    end, { desc = "[S]earch [/] in Open Files" })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set("n", "<leader>sn", function()
-      builtin.find_files({ cwd = vim.fn.stdpath("config") })
-    end, { desc = "[S]earch [N]eovim files" })
+    vim.keymap.set("n", "<leader>so", builtin.oldfiles, { desc = "[S]earch [O]ld" })
+    -- Grep
+    vim.keymap.set("n", "<leader>sg", function() builtin.current_buffer_fuzzy_find(dropdown) end, { desc = "[S]earch [G]rep Buffer" })
+    vim.keymap.set("n", "<leader>sG", function() builtin.live_grep({ prompt_title = "Grep Workspace" }) end, { desc = "[S]earch [G]rep Workspace" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch [W]ord" })
+    -- LSP
+    vim.keymap.set("n", "<leader>sr", builtin.lsp_references, { desc = "[S]earch [R]eferences" })
+    vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols Buffer" })
+    vim.keymap.set("n", "<leader>sS", builtin.lsp_workspace_symbols, { desc = "[S]earch [S]ymbols Workspace" })
+    vim.keymap.set("n", "<leader>si", builtin.lsp_implementations, { desc = "[S]earch [I]mplementations" })
+    vim.keymap.set("n", "<leader>sd", builtin.lsp_definitions, { desc = "[S]earch [D]efinitions" })
+    vim.keymap.set("n", "<leader>sD", builtin.lsp_type_definitions, { desc = "[S]earch Type [D]efinitions" })
+    vim.keymap.set("n", "<leader>st", builtin.treesitter, { desc = "[S]earch [T]reesitter" })
+    -- Misc
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end, { desc = "[S]earch [N]eovim files" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
   end,
 }
