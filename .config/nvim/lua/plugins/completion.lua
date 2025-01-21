eabbf1cf748b6082773df7cfe88dea9d7a78e798
Miguel Silva <miguelcesilva@gmail.com>
2025-01-21 11:42:22 +0000

    Blink, LSP, Plugin update

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,30 +1,20 @@
-return { -- Autocompletion
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
+return {
+  "saghen/blink.cmp",
+  dependencies = "rafamadriz/friendly-snippets",
+  version = "*",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+    },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
   },
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      completion = { completeopt = "menu,menuone,noinsert" },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-      }),
-      sources = {
-        {
-          name = "lazydev",
-          group_index = 0,
-        },
-        { name = "nvim_lsp" },
-        { name = "path" },
-      },
-    })
-  end,
+  opts_extend = { "sources.default" },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,11 +4,52 @@ return {
     { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "hrsh7th/cmp-nvim-lsp", -- Allows extra capabilities provided by nvim-cmp
+    "saghen/blink.cmp",
   },
-  config = function()
+  opts = {
+    servers = {
+      cssls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            ["ui.inlayhint.hints"] = {
+              compositeLiteralFields = true,
+              constantValues = true,
+              parameterNames = true,
+            },
+          },
+        },
+      },
+      html = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      },
+      pyright = {},
+    },
+  },
+  config = function(_, opts)
+    local lspconfig = require("lspconfig")
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    mason.setup()
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(opts.servers),
+      handlers = {
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
+      },
+    })
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
       callback = function(event)
         local telescope_builtin = require("telescope.builtin")
         vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, { buffer = event.buf, desc = "[G]oto [D]efinition" })
@@ -16,12 +57,10 @@ return {
         vim.keymap.set("n", "gr", telescope_builtin.lsp_references, { buffer = event.buf, desc = "[G]oto [R]eferences" })
         vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, { buffer = event.buf, desc = "[G]oto [I]mplementation" })
         vim.keymap.set("n", "H", vim.lsp.buf.hover, { buffer = event.buf, desc = "LSP Hover" })
-        vim.keymap.set("n", "<leader>cs", telescope_builtin.lsp_document_symbols, { buffer = event.buf, desc = "[C]ode [S]ymbols (Document)" })
-        vim.keymap.set("n", "<leader>cS", telescope_builtin.lsp_dynamic_workspace_symbols, { buffer = event.buf, desc = "[C]ode [S]ymbols (Workspace)" })
         vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = event.buf, desc = "[C]ode [R]ename" })
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "[C]ode [A]ction" })
         vim.api.nvim_set_keymap("n", "<leader>cd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "[C]ode [D]iagnostics" })
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
@@ -46,71 +85,12 @@ return {
           })
         end
 
-        -- The following code creates a keymap to toggle inlay hints in your
-        -- code, if the language server you are using supports them
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
           vim.keymap.set("n", "<leader>xh", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
           end, { desc = "[X]Toggle Inlay [H]ints" })
         end
       end,
-    })
-
-    --  Create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-          },
-        },
-      },
-      gopls = {
-        settings = {
-          gopls = {
-            ["ui.inlayhint.hints"] = {
-              compositeLiteralFields = true,
-              constantValues = true,
-              parameterNames = true,
-            },
-          },
-        },
-      },
-      -- html = {
-      --   format = {
-      --     templating = true,
-      --     wrapLineLength = 100,
-      --     wrapAttributes = 'auto',
-      --   },
-      --   hover = {
-      --     documentation = true,
-      --     references = true,
-      --   },
-      -- }
-    }
-
-    require("mason").setup()
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      "debugpy",
-      "gopls",
-      "lua-language-server",
-      "pyright",
-      "stylua",
-    })
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-    require("mason-lspconfig").setup({
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-          require("lspconfig")[server_name].setup(server)
-        end,
-      },
     })
   end,
 }
