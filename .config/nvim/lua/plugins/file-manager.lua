d571abd86e80024e3dfd00c38f97eacf5273248d
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2025-01-25 16:39:58 +0000

    Show hidden files (#59)

--- .config/nvim/lua/plugins/file-manager.lua
+++ .config/nvim/lua/plugins/file-manager.lua
@@ -1,25 +1,10 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", mode = "n", desc = "File [E]xplorer" },
-  },
   config = function()
-    local fb_actions = require("telescope").extensions.file_browser.actions
-
-    require("telescope").setup({
-      defaults = { --[[ your defaults]]
-      },
-      extensions = {
-        file_browser = {
-          ["n"] = {
-            c = fb_actions.create,
-            r = fb_actions.rename,
-            m = fb_actions.move,
-            y = fb_actions.copy,
-          },
-        },
-      },
-    })
+    local browser = require("telescope").extensions.file_browser
+    vim.keymap.set("n", "<leader>e", function()
+      browser.file_browser({ path = "%:p:h", select_buffer = true, hidden = true })
+    end, { desc = "File [E]xplorer" })
   end,
 }
