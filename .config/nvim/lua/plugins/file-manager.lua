6003203731f44a183192e9e87f9049957f03b266
Miguel Silva <miguelcesilva@gmail.com>
2025-03-30 12:50:26 +0100

    Add yazi and update packages

--- .config/nvim/lua/plugins/file-manager.lua
+++ .config/nvim/lua/plugins/file-manager.lua
@@ -1,10 +1,14 @@
 return {
-  "nvim-telescope/telescope-file-browser.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  config = function()
-    local browser = require("telescope").extensions.file_browser
-    vim.keymap.set("n", "<leader>e", function()
-      browser.file_browser({ path = "%:p:h", select_buffer = true, hidden = true })
-    end, { desc = "File [E]xplorer" })
-  end,
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  cmd = { "Yazi" },
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>Yazi cwd<cr>",
+      mode = "n",
+      desc = "[E]xplore directory",
+    },
+  },
+  opts = {},
 }
