2e949deed2f7ec82b7351a0f9936aef24e1f04e1
Miguel Silva <miguelcesilva@gmail.com>
2025-06-16 10:23:29 +0100

    Update plugins

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -12,7 +12,7 @@ return {
   },
   opts = {
     provider = "custom_claude_35_haiku",
-    vendors = {
+    providers = {
       custom_zendesk_o3_mini = {
         endpoint = "https://ai-gateway.zende.sk/v1/",
         model = "o3-mini-2025-01-31",
@@ -25,9 +25,11 @@ return {
         api_key_name = "AVANTE_ANTHROPIC_API_KEY",
         __inherited_from = "claude",
         timeout = 30000,
-        temperature = 0,
-        max_tokens = 8192,
         disable_tools = false,
+        extra_request_body = {
+          max_tokens = 8192,
+          temperature = 0,
+        },
       },
       custom_claude_35_sonnet = {
         endpoint = "https://api.anthropic.com",
@@ -35,9 +37,11 @@ return {
         api_key_name = "AVANTE_ANTHROPIC_API_KEY",
         __inherited_from = "claude",
         timeout = 120000,
-        temperature = 0,
-        max_tokens = 8192,
         disable_tools = false,
+        extra_request_body = {
+          max_tokens = 8192,
+          temperature = 0,
+        },
       },
       custom_claude_37_sonnet = {
         endpoint = "https://api.anthropic.com",
@@ -45,9 +49,11 @@ return {
         api_key_name = "AVANTE_ANTHROPIC_API_KEY",
         __inherited_from = "claude",
         timeout = 180000,
-        temperature = 0,
-        max_tokens = 8192,
         disable_tools = false,
+        extra_request_body = {
+          max_tokens = 8192,
+          temperature = 0,
+        },
       },
     },
   },
