854f1a4d1de64d9022b4649139fc575daa970f7c
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2025-07-13 15:24:17 +0100

    Go format with golanci-lint (#76)

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -14,12 +14,18 @@ return { -- Autoformat
   opts = {
     format_on_save = false,
     formatters_by_ft = {
-      go = { "gofmt", "goimports", "golines" },
+      go = { "golangcilint" },
       lua = { "stylua" },
       python = {
         "ruff_fix",
         "ruff_organize_imports",
         "ruff_format",
+      },
+    },
+    formatters = {
+      ["golangcilint"] = {
+        command = "go",
+        args = { "tool", "golangci-lint", "fmt", "--stdin" },
       },
     },
   },
