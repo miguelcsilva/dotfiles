a332c568d482a705b593d363b6dc630fcb118e9d
Miguel Silva <miguelcesilva@gmail.com>
2025-09-27 10:00:29 +0100

    Swap pyright for ty

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,19 +50,12 @@ return {
           },
         },
       },
-      pyright = {
-        cmd = { "pyright-langserver", "--stdio" },
+      ty = {
+        cmd = { "ty", "server" },
         filetypes = { "python" },
         root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
         settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              diagnosticMode = "workspace",
-              typeCheckingMode = "basic",
-            },
-          },
+          ty = {},
         },
       },
     }
