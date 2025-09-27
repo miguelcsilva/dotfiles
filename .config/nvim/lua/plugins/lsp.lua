5f6973cbbcaafc2badae27319c5eff73bd1c4b10
Miguel Silva <miguelcesilva@gmail.com>
2025-09-27 09:44:33 +0100

    Fix pyright lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -54,6 +54,16 @@ return {
         cmd = { "pyright-langserver", "--stdio" },
         filetypes = { "python" },
         root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+              typeCheckingMode = "basic",
+            },
+          },
+        },
       },
     }
 
