e54191fd7804faf1cbc2326a23da6c2187ff22c0
Miguel Silva <miguelcesilva@gmail.com>
2025-09-27 15:06:13 +0100

    Update git/which-key

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -15,11 +15,9 @@ return {
     },
     keys = {
       -- stylua: ignore start
-      { "<leader>if", "<cmd>DiffviewFileHistory --follow %<cr>", mode = "n", desc = "D[i]ff [F]ile" },
-      { "<leader>ir", "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>", mode = "n", desc = "D[i]ff [R]ange" },
-      { "<leader>il", "<Cmd>.DiffviewFileHistory --follow<CR>", mode = "n", desc = "D[i]ff [L]ine" },
-      { "<leader>io", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "D[i]ff [O]pen" },
-      { "<leader>ic", "<cmd>DiffviewClose<cr>", mode = "n", desc = "D[i]ff [C]lose" },
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "[G]it [D]iff open" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory --follow %<cr>", mode = "n", desc = "[G]it [H]istory" },
+      { "<leader>gc", "<cmd>DiffviewClose<cr>", mode = "n", desc = "[G]it [C]lose" },
       -- stylua: ignore end
     },
   },
@@ -31,14 +29,12 @@ return {
     },
       -- stylua: ignore start
     keys = {
-      { "<leader>hr", function() require("gitsigns").reset_hunk() end, mode = "n", desc = "[H]unk [R]eset" },
-      { "<leader>hr", function() require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, mode = "v", desc = "[H]unk [R]eset" },
-      { "<leader>hR", function() require("gitsigns").reset_buffer() end, mode = "n", desc = "[H]unk [R]eset Buffer" },
-      { "<leader>hn", function() require("gitsigns").nav_hunk("next") end, mode = "n", desc = "[H]unk [N]ext" },
-      { "<leader>hp", function() require("gitsigns").nav_hunk("prev") end, mode = "n", desc = "[H]unk [P]rev" },
-      { "<leader>hb", function() require("gitsigns").blame_line() end, mode = "n", desc = "[H]unk [B]lame" },
-      { "<leader>hB", function() require("gitsigns").blame_line({full = true}) end, mode = "n", desc = "[H]unk [B]lame Full" },
-      { "<leader>hd", function() require("gitsigns").diffthis() end, mode = "n", desc = "[H]unk [D]iff" },
+      { "<leader>gr", function() require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, mode = "v", desc = "[G]it [R]eset line" },
+      { "<leader>gR", function() require("gitsigns").reset_buffer() end, mode = "n", desc = "[G]it [R]eset buffer" },
+      { "<leader>gn", function() require("gitsigns").nav_hunk("next") end, mode = "n", desc = "[G]it [N]ext hunk" },
+      { "<leader>gp", function() require("gitsigns").nav_hunk("prev") end, mode = "n", desc = "[G]unk [P]rev hunk" },
+      { "<leader>gb", function() require("gitsigns").blame_line() end, mode = "n", desc = "[H]unk [B]lame" },
+      { "<leader>gB", function() require("gitsigns").blame_line({full = true}) end, mode = "n", desc = "[H]unk [B]lame Full" },
       -- stylua: ignore end
     },
   },
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -3,12 +3,10 @@ return {
   event = "VimEnter", -- Sets the loading event to 'VimEnter'
   opts = {
     spec = {
-      { "<leader>b", group = "[B]uffer" },
       { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
       { "<leader>d", group = "[D]ebug" },
       { "<leader>s", group = "[S]earch" },
-      { "<leader>t", group = "[T]est" },
-      { "<leader>h", group = "[H]unk" },
+      { "<leader>h", group = "[G]it" },
       { "<leader>x", group = "[X]Toggle" },
     },
   },
