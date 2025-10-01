beed8ef550704e88a3ef1fe85d6e767c1842ebc7
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2025-10-01 15:09:20 +0100

    Replace ty with pyright (#89)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,12 +50,19 @@ return {
           },
         },
       },
-      ty = {
-        cmd = { "ty", "server" },
+      ["pyright"] = {
+        cmd = { "pyright-langserver", "--stdio" },
         filetypes = { "python" },
         root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+        name = "pyright",
         settings = {
-          ty = {},
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "openFilesOnly",
+            },
+          },
         },
       },
     }
